4a72b7e1628404378e2fb232d312c6721ce5d4c5
UJGunner <122560296+UJGunner@users.noreply.github.com>
2023-02-07 08:11:08 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,28 +6,61 @@ function score_update () {
     OLED.newLine()
     OLED.writeStringNewLine("rounds:" + rounds)
 }
-input.onButtonPressed(Button.A, function () {
-    sprite.change(LedSpriteProperty.X, -1)
-})
-input.onButtonPressed(Button.AB, function () {
-    sprite.change(LedSpriteProperty.Y, -1)
-})
-input.onButtonPressed(Button.B, function () {
-    sprite.change(LedSpriteProperty.X, 1)
-})
-input.onGesture(Gesture.Shake, function () {
-    sprite.change(LedSpriteProperty.Y, 1)
+function speed_change () {
+    if (speed == 1) {
+        basic.pause(500)
+        basic.pause(200)
+        basic.pause(100)
+    } else if (speed == 2) {
+        basic.pause(500)
+        basic.pause(100)
+    } else if (speed == 3) {
+        basic.pause(200)
+        basic.pause(200)
+    } else if (speed == 4) {
+        basic.pause(200)
+    } else if (speed == 0) {
+        basic.pause(1000)
+    } else {
+        basic.pause(100)
+    }
+}
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    if (speed == 5) {
+        speed = 0
+    } else {
+        speed += 1
+    }
 })
 let sprite_2: game.LedSprite = null
-let sprite: game.LedSprite = null
 let score = 0
 let lives = 0
 let rounds = 0
+let speed = 0
 OLED.init(128, 64)
+speed = 0
 rounds = 0
 lives = 3
 score = 0
-sprite = game.createSprite(2, 2)
+let sprite = game.createSprite(2, 2)
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltRight)) {
+        sprite.change(LedSpriteProperty.X, 1)
+        speed_change()
+    }
+    if (input.isGesture(Gesture.TiltLeft)) {
+        sprite.change(LedSpriteProperty.X, -1)
+        speed_change()
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        sprite.change(LedSpriteProperty.Y, 1)
+        speed_change()
+    }
+    if (input.isGesture(Gesture.LogoDown)) {
+        sprite.change(LedSpriteProperty.Y, -1)
+        speed_change()
+    }
+})
 basic.forever(function () {
     if (0 < lives) {
         sprite_2 = game.createSprite(randint(0, 4), randint(0, 4))
@@ -51,6 +84,7 @@ basic.forever(function () {
         sprite_2.delete()
     } else {
         sprite.delete()
+        score_update()
         basic.showString("Game over")
     }
 })
